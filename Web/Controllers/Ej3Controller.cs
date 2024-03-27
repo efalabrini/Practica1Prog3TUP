e880c9a0948dfa720095e82945146e01fc23eb96
Danilo Mercado <mercadodanilo213@hotmail.com>
2024-03-27 14:45:15 -0300

    Ejercicio 3 resuelto

---
+++ Web/Controllers/Ej3Controller.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Ej3Controller: ControllerBase
+    {
+        [HttpGet()]
+        public string Get([FromQuery] string name, [FromQuery] string age) 
+        {
+            string result = $"Te llamas {name} y tiene {age} años!!!!";
+            return result;
+        }
+
+    }
+}
+
+
+//3) Pedir por consola tu nombre y tu edad y mostrar el siguiente mensaje: “Te llamas ” <nombre> ” y tienes ” <años> ” años”
