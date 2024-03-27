348ca761588b172ca62588e42a8de786481e5673
Priscila Di Renzo <prisciladirenzo@gmail.com>
2024-03-27 14:45:02 -0300

    resolucion ejercicio 3

---
+++ Web/Controllers/Ej3Controller.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Ej3Controller : ControllerBase
+    {
+        [HttpGet()]
+        public string Get([FromQuery] string nombre, [FromQuery] string edad [FromQuery])
+        {
+
+            string mensaje = $"Te llamas {nombre} y tenés {edad}";
+
+            return mensaje;
+        }
+    }
+}
