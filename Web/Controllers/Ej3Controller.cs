fd217ccb840d7bae3b18b8bbf486624101cc39c6
Valentino Maffeis <valentinomaffeis02@gmail.com>
2024-03-27 15:01:14 -0300

    ejercicio 3 resuelto

---
+++ Web/Controllers/Ej3Controller.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Controllers
+{
+    public class Ej3Controller
+    {
+        [HttpGet()]
+        public string Get([FromQuery] string nombre, [FromQuery] string edad)
+        {
+
+            string result = $"Hola su nombre es {nombre} y tiene {edad} anios";
+            return result;
+        }
+    }
+}
