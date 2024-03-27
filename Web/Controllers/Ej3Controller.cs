95d7dc403e350e00ea7f561ac46dc6db26c82ae8
Pilar Lopez <136598559+PilarLopez17@users.noreply.github.com>
2024-03-27 17:04:26 -0300

    Update Ej3Controller.cs

--- Web/Controllers/Ej3Controller.cs
+++ Web/Controllers/Ej3Controller.cs
@@ -1,19 +1,1 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
 
-namespace Web.Controllers
-{
-    [Route("api/[controller]")]
-    [ApiController]
-
-
-    public class Ej3Controller : ControllerBase
-    {
-        [HttpGet()]
-        public string Get([FromQuery] string nombre, [FromQuery] string anios)
-        {
-            string result = $"Te llamas {nombre} y tienes {anios} años";
-            return result;
-        }
-    }
-}
