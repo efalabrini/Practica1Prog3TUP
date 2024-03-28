5783da0f301fb3c01cfa3d8cf6baad69c0bacf53
FrancoBalduini <franco233433@gmail.com>
2024-03-28 00:33:35 -0300

    Resolucion Ejercicio 6

---
+++ Web/Controllers/Ej6Controller.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Ej6Controller : ControllerBase
+    {
+        [HttpGet()]
+        public string Get([FromQuery] int price, [FromQuery] string pay, [FromQuery] string numCard)
+        {
+            
+            if (price <= 0)
+            {
+                return "Error: El precio del producto debe ser un valor positivo.";
+            }
+
+            double Total = price; 
+
+            
+            if (pay.ToUpper() == "TARJETA")
+            {
+                
+                if (numCard.Length != 16)
+                {
+                    return "Error: El número de tarjeta debe contener 16 dígitos.";
+                }
+
+                
+                Total *= 1.10; 
+            }
+
+            return $"El valor a pagar es: {Total}";
+        }
+    }
+}
