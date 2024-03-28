e3cf6fef321c650b415f7345143d561ebbbf2f34
JuaniTorti <nachi.tortjuani@gmail.com>
2024-03-28 16:09:44 -0300

    Create Ej9Controller.cs

---
+++ Web/Controllers/Ej9Controller.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+
+
+    public class Ej9Controller : ControllerBase
+    {
+        [HttpGet()]
+        public Array Get()
+        {
+            List<int> pairNumbers = new();
+
+            for (int i = 1; i <= 100; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    pairNumbers.Add(i);
+                }
+            }
+
+
+            return pairNumbers.ToArray(); // no me dejaba retornar una lista asiq transformo la lista en array y la retorno
+        }
+    }
+}
