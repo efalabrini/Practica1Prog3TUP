692e7e6aedbbc92e5a02dbe01c562a678dc69408
facundolgomez <facundolgomez87@gmail.com>
2024-03-28 00:23:48 -0300

    ejercicio5 resuelto

---
+++ Web/Controllers/Ej5Controller.cs
@@ -0,0 +1,35 @@
+﻿using System.Security.Cryptography;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Ej5Controller
+    {
+        [HttpGet()]
+        public string Get([FromQuery] string day)
+        {
+            string daySelected = day.Trim();
+            switch (daySelected.ToLower())
+            {
+                case "lunes":
+                case "martes":
+                case "miércoles":
+                case "jueves":
+                    return "no";
+                case "viernes":
+                case "sábado":
+                case "domingo":
+                    return "yes";
+                default:
+                    return "día no válido";
+            }
+
+        }
+    }
+}
+
+
+
