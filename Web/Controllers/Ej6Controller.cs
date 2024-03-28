d8f1ea819ae21a958ff177810be75b5fa4eb683e
facundolgomez <facundolgomez87@gmail.com>
2024-03-28 01:27:24 -0300

    subiendo ejercicio 6

---
+++ Web/Controllers/Ej6Controller.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Ej6Controller 
+    {
+        [HttpGet]
+        public string Get([FromQuery] double price, [FromQuery] string pay, [FromQuery] string card = "")
+        {
+            if (price <= 0)
+            {
+                return "el precio tiene que ser positivo";
+            }
+
+            string typeOfPay = pay.Trim().ToLower();
+
+            if (typeOfPay == "tarjeta")
+            {
+
+                string valueOfCard = card.Trim();
+                if (valueOfCard.Length != 16)
+                {
+                    return "la longitud tiene que ser de 16 digitos";
+                }
+                price *= 1.1;
+            }
+
+            return $"El valor a pagar es: {price}";
+        }
+    }
+}
+
+
