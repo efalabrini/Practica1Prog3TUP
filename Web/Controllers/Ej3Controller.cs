ab93e5cf9d57e651ea97778629989f0500fdf29f
Agustin Castelli <rev0.c4chomax@gmail.com>
2024-03-28 19:52:12 -0300

    Ejercicio3-nameAndAge

--- Web/Controllers/Ej3Controller.cs
+++ Web/Controllers/Ej3Controller.cs
@@ -1,6 +1,17 @@
-﻿namespace Web.Controllers
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Controllers
 {
-    public class Ej3Controller
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Ej3Controller : ControllerBase
     {
+        [HttpGet()]
+        public string Get([FromQuery] string name, [FromQuery] string age)
+        {
+            string nameAndAge = $"Tu nombre es {name} y tienes {age} años.";
+            return nameAndAge;
+        }
     }
 }
