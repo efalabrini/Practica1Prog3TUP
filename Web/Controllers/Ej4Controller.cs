24e39b1a3cfd834f7c98c94db9deef6b81c926e5
Agustin Castelli <rev0.c4chomax@gmail.com>
2024-03-28 20:47:01 -0300

    Ejercicio 4 Commit

---
+++ Web/Controllers/Ej4Controller.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Ej4Controller : ControllerBase
+    {
+        [HttpGet()]
+        public string Get([FromQuery] int num1, [FromQuery] int num2) 
+        { 
+            if (num1 > num2)
+            {
+                return $"{num1} es el mayor de los números ingresados.";
+            }
+            else
+            {
+                if (num2 > num1)
+                {
+                    return $"{num2} es el mayor de los números ingresados."; ;
+                }
+                else
+                {
+                    return $"Los números ingresados son iguales."; ;
+                }
+            }
+           
+        }
+    }
+}
