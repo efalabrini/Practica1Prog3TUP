89f5e1c5597e4fce58e411e65d849894c63edc6f
FrancoBalduini <franco233433@gmail.com>
2024-03-27 23:56:45 -0300

    Resolucion Ejercicio 5

---
+++ Web/Controllers/Ej5Controller.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Ej5Controller : ControllerBase
+    {
+        [HttpGet()]
+        public string Get([FromQuery] string day)
+        {
+            day = day.Trim();
+
+            switch (day)
+            {
+                case "Lunes":
+                case "Martes":
+                case "Miércoles":
+                case "Jueves":
+                case "Viernes":
+                    string dayN = "No es fin de semana";
+                    return dayN;
+                case "Sábado":
+                case "Domingo":
+                    string dayS = "Es fin de semana";
+                    return dayS;
+                default:
+                    string dayError = "Error Inválido.";
+                    return dayError;
+            }
+        }
+    }
+}
