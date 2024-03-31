c2914ffdc51ff0c1edcd20c60bf1ed89ba679c28
Nicolas Arrastia <nicolasarrastia04@gmail.com>
2024-03-31 17:58:47 -0300

    Ejercicio7 bucle for

---
+++ Web/Controllers/Ej7controller.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Ej7Controller : ControllerBase
+    {
+        [HttpGet()]
+        public List<int> Get()
+        {
+            List<int> numbers = new List<int>();
+
+            for (int i = 1; i <= 100; i++)
+            {
+                numbers.Add(i);
+            }
+            return numbers;
+        }
+    }
+}
