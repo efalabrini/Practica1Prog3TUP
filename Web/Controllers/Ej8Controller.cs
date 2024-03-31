e730354a278350bb091ae34dff407ba02dcccf5a
Valentino Cueto <valentinocueto2005@gmail.com>
2024-03-31 16:53:07 -0300

    Ejercicios resueltos

---
+++ Web/Controllers/Ej8Controller.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Ej8Controller : ControllerBase
+    {
+        [HttpGet()]
+        public List<int> Get()
+        {
+            List<int> numbers = new List<int>();
+            int i = 1;
+            while (i <= 100)
+            {
+                numbers.Add(i);
+                i++;
+            }
+            return numbers;
+        }
+    }
+}
