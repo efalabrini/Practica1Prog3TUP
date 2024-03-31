bcb0b71ef84807a62a3bd827374e7175c77a7b93
Nicolas Arrastia <nicolasarrastia04@gmail.com>
2024-03-31 18:04:32 -0300

    ejercicio8 bucle-while

---
+++ Web/Controllers/Ej8Controller.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Ej8Controller : ControllerBase
+    {
+        [HttpGet()]
+        public List<int> Get()
+        {
+            List<int> bucleNumbers= new List<int>();
+            int i = 1;
+            while (i <= 100)
+            {
+                bucleNumbers.Add(i);
+                i++;
+            }
+            return bucleNumbers;
+        }
+    }
+ }
+
