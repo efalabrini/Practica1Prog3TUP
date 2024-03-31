39f00fcb9c26587ba5208c1ed7224449aa476608
Nicolas Arrastia <nicolasarrastia04@gmail.com>
2024-03-31 18:10:37 -0300

    Ejericio9 pairs numbers

---
+++ Web/Controllers/Ej9Controller.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Ej9Controller : ControllerBase
+    {
+        [HttpGet()]
+        public List<int> Get()
+        {
+            List<int> pairsNumber = new List<int>();
+            
+            for (int i = 1;i <= 100; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    pairsNumber.Add(i);
+                }
+            }
+            return pairsNumber;
+        }
+    }
+}
