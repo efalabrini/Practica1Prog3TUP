771b0450a6d3ba74982b91344756e02bd26b0352
Nachobastianelli <nachobastianelli2003@gmail.com>
2024-04-02 18:20:23 -0300

    Ejercicio5-DiasDeLaSemana

---
+++ Web/Controllers/Ej4Controller.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Ej4Controller : ControllerBase
+    {
+        [HttpGet()]
+        public string Get([FromQuery] int var1, [FromQuery] int var2)
+        {
+            string response = "Son iguales";
+            if (var1 > var2) 
+            {
+                response = $"El valor {var1} es el mayor";
+            }
+            if (var2 > var1)
+            {
+                response = $"El valor {var2} es el mayor";
+            }
+            return response;
+        }
+    }
+}
+
+//4) Pedir dos números al usuario por teclado y decir qué número es el mayor.
+
---
+++ Web/Controllers/Ej5Controller .cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Ej5Controller : ControllerBase
+    {
+        [HttpGet()]
+        public string Get([FromQuery] string day)
+        {
+            string response = "Es fin de semana";
+            day = day.ToLower();
+            string[] week = new string[] { "lunes", "martes", "miércoles", "jueves", "viernes" , "sabado", "domingo"};
+            
+             bool containsDaysWeek = week.Any(days =>
+                day.Trim().Contains(days, StringComparison.OrdinalIgnoreCase));
+            if (containsDaysWeek & (day.Trim() == "sabado" | day.Trim() == "domingo"))
+            {
+                response = "Fin de semana";
+            }
+            else if (containsDaysWeek)
+            {
+                response = "Es un dia de semana";
+            }
+            else
+            {
+                response = "Expresion invalida---====09099_+++)][[][";
+            }
+           
+            return response;
+
+        }
+    }
+} 
+
+//5) Pedir el nombre del día al usuario y decirle si es fin de semana o no.  En caso de error, indicarlo. Se debe poder ingresar, por ejemplo, “Lunes” / “LUNES” / “lunes” / “lUnes” /
+//“  lunes” y debería funcionar. TIP para investigar: Trim() y StringComparison.OrdinalIgnoreCase
+
+
+
