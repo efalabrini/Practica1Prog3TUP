b6c4261b81333ff57b0e11231b67390d70a2a706
alanmoex <alan.moex@gmail.com>
2024-04-03 14:14:34 -0300

    Create Ej5Controller.cs

---
+++ Web/Controllers/Ej5Controller.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Ej5Controller : ControllerBase
+    {
+        [HttpGet()]
+        public string Get([FromQuery] string inputDay)
+        {
+            string response;
+            string[] weekDays = ["lunes", "martes", "miercoles", "jueves", "viernes"];
+            string[] weekEndDays = ["sabado", "domingo"];
+
+            inputDay = inputDay.Trim();
+
+            if (Array.Exists(weekEndDays, day => day.Equals(inputDay, StringComparison.OrdinalIgnoreCase)))
+            {
+                response = "Es fin de semana";
+            }
+            else if (Array.Exists(weekDays, day => day.Equals(inputDay, StringComparison.OrdinalIgnoreCase)))
+            {
+                response = "No es fin de semana";
+            }else 
+            {
+                response = "Error. Por favor, ingrese un dia de la semana";
+            }
+
+            return response;
+        }
+    }
+}
