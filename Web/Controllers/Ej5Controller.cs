28e78ccb18473cc2eb69f767058b4d38a13d1147
facundolgomez <facundolgomez87@gmail.com>
2024-04-03 20:49:55 -0300

    subiendo ej5

---
+++ Web/Controllers/Ej5Controller.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Ej5Controller
+    {
+        [HttpGet()]
+        public string Get([FromQuery] string day)
+        {
+            string daySelected = day.Trim();
+            switch (daySelected.ToLower())
+            {
+                case "lunes":
+                case "martes":
+                case "miércoles":
+                case "jueves":
+                    return "no";
+                case "viernes":
+                case "sábado":
+                case "domingo":
+                    return "yes";
+                default:
+                    return "día no válido";
+            }
+
+        }
+    }
+}
+
