426670fcbab2492e8dc79438a26955bfb9731e38
MarianoGerosa <gerosamariano@gmail.com>
2024-04-03 14:22:18 -0300

    Ejercicio 5/Pedir-Dia

---
+++ Web/Controllers/Ej5Controller.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Ej5Controller : ControllerBase
+    {
+        [HttpGet()]
+        public string Get([FromQuery] string day)
+        {
+            string[] dayWeek = ["lunes", "martes", "miercoles", "jueves", "viernes"];
+            string[] dayWeekend = ["sabado", "domingo"];
+
+            day = day.ToLower().Trim();
+
+            if (dayWeek.Contains(day)) 
+            {
+                return "El dia ingresado NO pertenece al fin de semana";
+            }
+            else if (dayWeekend.Contains(day)) 
+            {
+                return "El dia ingresado Si pertenece al fin de semana";
+            }
+
+            return "ERROR. No se ingreso un dia";
+        }
+    }
+}
