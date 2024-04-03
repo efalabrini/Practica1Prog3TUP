52fbfe1f2f5c3e2c4b7d9b3bd18bf1e8b68311d9
Priscila Di Renzo <prisciladirenzo@gmail.com>
2024-04-03 20:53:56 -0300

    ejercicio3-resuelto-pedir-edad

---
+++ Web/Controllers/Ej3Controller.cs
@@ -0,0 +1,19 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Ej3Controller : ControllerBase
+    {
+        [HttpGet()]
+        public string Get([FromQuery] string nombre, [FromQuery] string edad)
+        {
+
+            string mensaje = $"Te llamas {nombre} y tenés {edad}";
+
+            return mensaje;
+        }
+    }
+}
