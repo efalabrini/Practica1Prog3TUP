5773480823ff4c80b869a5358ca3883336c4c8ab
Priscila Di Renzo <prisciladirenzo@gmail.com>
2024-04-03 07:43:06 -0300

    ejercicio4-num-mayor

---
+++ Web/Controllers/Ej4Controller.cs
@@ -0,0 +1,35 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Ej4Controller : ControllerBase
+    {
+        [HttpGet()]
+        public string Get([FromQuery] string num1, [FromQuery] string num2)
+        {
+            string resultado;
+   
+            if (int.Parse(num1) > int.Parse(num2))
+            {
+                resultado = $"El número mayor es: {num1}";
+            }
+            else if (int.Parse(num2) > int.Parse(num1))
+            {
+                resultado = $"El número mayor es: {num2}";
+            }
+            else
+            {   
+                resultado = "Los números son iguales";
+
+            }
+
+            return resultado;
+
+
+
+        }
+    }
+}
