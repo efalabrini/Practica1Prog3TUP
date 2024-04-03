b2f0a5c58c8974930cad65991ad3d2b7e4d5ee11
facundolgomez <facundolgomez87@gmail.com>
2024-04-03 20:24:48 -0300

    subiendo ej4

---
+++ Web/Controllers/Ej4Controller.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Ej4Controller
+    {
+        [HttpGet()]
+        public string Get([FromQuery] int num1, [FromQuery] int num2)
+        {
+            if (num1 > num2)
+            {
+                return $"{num1} es mayor que {num2}";
+            }
+            else if (num1 < num2)
+            {
+                return $"{num2} es mayor que {num1}";
+            }
+            else
+            {
+                return "Son iguales";
+            }
+        }
+    }
+}
