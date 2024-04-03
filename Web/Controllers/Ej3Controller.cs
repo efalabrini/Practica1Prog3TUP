8f5e8bdb6c2ec006a4d83db21319b1bb36b05dc0
facundolgomez <facundolgomez87@gmail.com>
2024-04-03 18:46:40 -0300

    subiendo ej3

---
+++ Web/Controllers/Ej3Controller.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Ej3Controller
+    {
+        [HttpGet()]
+        public string Get([FromQuery] string nombre, [FromQuery] string anios)
+        {
+            string resultado = $"Te llamas {nombre} y tiene {anios} anios";
+            return resultado;
+
+
+
+        }
+
+    }
+}
