e7efb7181c144acfdbfa46ff29a8027cec4802f4
AugustoMikbel <agussgabriel8@gmail.com>
2024-04-03 17:50:03 -0300

    create ejercicio4

---
+++ Web/Controllers/ej4.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+namespace Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ej4 : ControllerBase
+    {
+        [HttpGet()]
+        public string Get([FromQuery] int nro1, [FromQuery] int nro2)
+
+        {
+            if (nro1 > nro2)
+            {
+                return $"El nro mayor es el nro1 {nro1}";
+
+
+            }
+            else if (nro1 == nro2)
+                {
+                return $"El nro 1 {nro1} es igual al nro2 {nro2}";
+                }
+            else
+            {
+                return $"El nro mayor es el nro2 {nro2}";
+            }
+
+
+
+        }
+
+    }
+}
