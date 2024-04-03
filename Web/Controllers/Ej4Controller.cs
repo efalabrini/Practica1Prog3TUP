d235f63dfc29b280a85e01ca12b3344ab600eaec
Nicolas Cataldi <nicolascataldi1@gmail.com>
2024-04-03 13:53:38 -0300

    Ejercicio 4 Hecho

---
+++ Web/Controllers/Ej4Controller.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Ej4Controller : ControllerBase
+    {
+        [HttpGet()]
+        public string Get([FromQuery] int num1, [FromQuery] int num2)
+        {
+
+            string mensaje;
+
+            if (num1 > num2)
+            {
+                mensaje = $"{num1} es mayor a {num2}";
+            }
+            else if(num1 < num2){
+                mensaje = $"{num2} es mayor a {num1}";
+            }
+            else
+            {
+                mensaje = "Ambos numeros son iguales";
+            }
+
+            return mensaje;
+
+        }
+    }
+}
