221c17a42c42dc9b880c9cbcb281ed0fc0225df2
facundolgomez <facundolgomez87@gmail.com>
2024-04-03 22:04:26 -0300

    ejercicio8 resuelto

---
+++ Web/Controllers/Ej8Controller.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Ej8Controller: ControllerBase
+    {
+        [HttpGet()]
+        public List<int> Get() { 
+            List <int> list = new List<int>();
+            int x = 1;
+            while (x < 101) {
+                list.Add(x);
+                x++;
+            }
+            return list;
+        
+        }
+        
+    }
+}
+
+
+
