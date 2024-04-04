bdf46f9952a366175b84f73d7d4e41cc000170cf
facundolgomez <facundolgomez87@gmail.com>
2024-04-03 21:34:50 -0300

    ej7 resuelto

---
+++ Web/Controllers/Ej7Controller.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Ej7Controller : ControllerBase
+    {
+        [HttpGet()]
+        public List<int> Get()
+        {
+            List<int> myList = new List<int>(); 
+            
+            for(int i = 1; i < 101; i++) { 
+                myList.Add(i);
+            }
+            return myList;
+        }
+
+            
+
+    }
+}
+
+
