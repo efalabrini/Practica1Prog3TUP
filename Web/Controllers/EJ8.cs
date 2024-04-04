2ffd1dbae647f4cdda444c1b7a7cd017c1430f91
AugustoMikbel <agussgabriel8@gmail.com>
2024-04-04 09:35:59 -0300

    createej8

---
+++ Web/Controllers/EJ8.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+namespace Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EJ8 : ControllerBase
+    {
+
+        [HttpGet()]
+        public string Get()
+        {
+            int numero = 0;
+            while (numero != 100)
+            {
+                numero++;
+                
+            }
+            return $"el numero es {numero}";
+
+        }
+    }
+}
