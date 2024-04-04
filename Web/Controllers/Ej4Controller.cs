bcdf912aa5ec438c04c12738bc6ec10ea58d6d93
Aylen Guy <ayluguy@gmail.com>
2024-04-03 22:29:40 -0300

    Ejericio 4 resuelto

---
+++ Web/Controllers/Ej4Controller.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EJ4Controller : ControllerBase
+    {
+        [HttpGet()]
+        public string Get([FromQuery] int number1, [FromQuery] int number2)
+        {
+            if (number1 > number2)
+            {
+                return $"El número mayor es {number1}";
+            }
+            else if (number1 < number2)
+            {
+                return $"El número mayor es {number2}";
+
+            }
+
+            return $"Los numeros son iguales";
+
+
+
+        }
+
+    }
+}
