7d89bb0b3e6099c024fa545b1c06dceda3b37dd8
facundolgomez <facundolgomez87@gmail.com>
2024-04-04 23:30:55 -0300

    ej3Linq resuelto

---
+++ Web/Controllers/Ej3LinqController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Ej3LinqController : ControllerBase
+    {
+        [HttpGet()]
+        public string[] Get([FromQuery] string[] queryString)
+        {
+            var filteredWords = queryString.Where(word => word.ToLower().StartsWith('b') && word.ToLower().EndsWith('r')).ToArray();
+            return filteredWords;
+
+            
+
+
+        }
+    }
+}
