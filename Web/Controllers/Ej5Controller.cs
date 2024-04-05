dc65c759911069ef5f76e69898566643925af232
Julian Paquez <julianpaquez@gmail.com>
2024-04-05 13:32:39 -0300

    Ejercicio 5 resuelto

---
+++ Web/Controllers/Ej5Controller.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Ej5Controller : ControllerBase
+    {
+        [HttpGet()]
+        public string Get([FromQuery] string nombreDia)
+        {
+            string[] diaSemana = ["lunes", "martes", "miercoles", "jueves", "viernes"];
+            string[] finSemana = ["sabado", "domingo"];
+            
+            nombreDia = nombreDia.ToLower().Trim();
+
+            if(diaSemana.Contains(nombreDia))
+            {
+                return "Ese día es un día de la semana";
+
+            }else if (finSemana.Contains(nombreDia))
+            {
+                return "Ese día es un día del fin de semana";
+            }
+
+            return "Incorrecto.Usted ha ingresado un dia que no coincide con ningún dia existente";
+        }
+    }
+}
+
+
+
+
+
+
