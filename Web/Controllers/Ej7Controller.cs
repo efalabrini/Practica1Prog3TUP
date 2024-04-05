227347774c290ca98d97fa753f8863894c97a0ef
Lucaslovera24 <lucaslovera24@hotmail.com>
2024-04-05 14:49:01 -0300

    Ejercicio 7 terminado

---
+++ Web/Controllers/Ej7Controller.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Text;
+
+namespace Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+
+    public class Ej7Controller : ControllerBase
+    {
+        [HttpGet()]
+        public string Get()
+        {
+            StringBuilder numeros = new StringBuilder();
+            for (int i = 0; i <= 100; i++)
+            {
+                numeros.Append(i.ToString());
+                numeros.Append(", ");
+            }
+            return numeros.ToString();
+        }
+    }
+}
