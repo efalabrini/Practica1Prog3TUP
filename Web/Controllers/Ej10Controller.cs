35ec0cf1471b800505cb36b9a6300a7905a362bf
Julian Paquez <julianpaquez@gmail.com>
2024-04-05 14:33:21 -0300

    Ejercicio 10 resuelto

---
+++ Web/Controllers/Ej10Controller.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Ej10Controller : Controller
+    {
+        [HttpGet]
+        public string Get()
+        {
+            List<int> numPares = [];
+            List<int> numDivisibles3 = [];
+
+            for (int i = 0; i <= 100; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    numPares.Add(i);
+
+                    if (i % 3 == 0)
+                    {
+                        numDivisibles3.Add(i);
+                    }
+                }
+                else if (i % 3 == 0)
+                {
+                    numDivisibles3.Add(i);
+                }
+            }
+
+            return $"Los numeros pares son: {string.Join(", ", numPares)}\n Los numeros divisibles por 3 son {string.Join(", ", numDivisibles3)}";
+
+        }
+    }
+}
