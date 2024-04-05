aef233d66ff6e081da14dd351800f35399ce10bc
Julian Paquez <julianpaquez@gmail.com>
2024-04-05 13:12:28 -0300

    Ejercicio 4 resuelto

---
+++ Web/Controllers/Ej4Controller.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Ej4Controller : ControllerBase
+    {
+        [HttpGet()]
+        public string Get([FromQuery] int numero1, [FromQuery] int numero2)
+        {
+            if(numero1 > numero2)
+            {
+                string result = $"{numero1} es el numero mas grande";
+
+                return result;
+
+            }else
+            {
+                string result = $"{numero2} es el numero mas grande";
+                return result;
+            }
+        }
+    }
+}
