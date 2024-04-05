beb7afb3d7e99803344342c130266c35cb0791f4
facundolgomez <facundolgomez87@gmail.com>
2024-04-05 00:09:41 -0300

    ej4Linq resuelto

---
+++ Web/Controllers/Ej4LinqController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Ej4LinqController : ControllerBase
+    {
+        [HttpGet()]
+        public int[] Get([FromQuery] int[] numbers)
+        {
+            var selectedNumbers = numbers.OrderByDescending(x => x).Take(5).ToArray();
+            return selectedNumbers;
+
+
+        }
+    }
+}
+
