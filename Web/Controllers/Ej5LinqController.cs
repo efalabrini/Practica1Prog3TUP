893d90af8b42f6af54eaa194adb5c993c117b379
facundolgomez <facundolgomez87@gmail.com>
2024-04-05 02:25:03 -0300

    ej5Linq resuelto

---
+++ Web/Controllers/Ej5LinqController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Ej5LinqController : ControllerBase
+    {
+        [HttpGet()]
+        public string[] Get([FromQuery] int[] queryNumbers)
+        {
+            var numbers = queryNumbers.Select(x => Math.Pow(x, 2)).ToArray();
+            List<string> numbersAndSquare = new List<string>();
+
+            for (int i = 0; i <numbers.Length; i++)
+            {
+                if (numbers[i] > 20)
+                {
+                    numbersAndSquare.Add($"{queryNumbers[i]}-{numbers[i]}");
+                }
+            }
+            return numbersAndSquare.ToArray();
+
+
+        }
+    }
+}
+
+
+
+
+
+
