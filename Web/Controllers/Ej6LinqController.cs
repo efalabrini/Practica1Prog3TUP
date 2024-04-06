b05eadef584f6393668cbbee722e749edb50c2a0
facundolgomez <facundolgomez87@gmail.com>
2024-04-06 01:22:39 -0300

    ej6Linq resuelto

---
+++ Web/Controllers/Ej6LinqController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Ej6LinqController : ControllerBase
+    {
+        [HttpGet()]
+        public string[] Get([FromQuery] List<string> words)
+        {
+            var replacedWords = words.Select(x => x.ToLower().Replace("ia", "*")).ToArray();
+            return replacedWords;
+
+
+        }
+    }
+}
+
+
+
+
+
+
+
+
