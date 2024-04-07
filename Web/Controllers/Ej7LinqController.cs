8d800cde94d710f298f98b9ca41edccb98f00b74
JuaniTorti <nachi.tortjuani@gmail.com>
2024-04-07 11:59:34 -0300

    Create Ej7LinqController.cs

---
+++ Web/Controllers/Ej7LinqController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Ej7LinqController : ControllerBase
+    {
+        [HttpGet()]
+        public IList<string> Get([FromQuery] string cadena)
+        {
+            
+           return DevolverPalabrasCapitales(cadena);
+
+        }
+
+        public static IList<string> DevolverPalabrasCapitales(string cadena)
+        {
+            var listWords = cadena.Split().ToList();
+            return listWords.Where(word => word == word.ToUpper()).ToList();
+        }
+    }
+}
