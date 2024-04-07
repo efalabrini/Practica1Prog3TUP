0d2954c1901d7ed4b13c60a7f5502e458ffe94a9
JuaniTorti <nachi.tortjuani@gmail.com>
2024-04-07 10:44:58 -0300

    Create Ej4LinqController.cs

---
+++ Web/Controllers/Ej4LinqController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Ej4LinqController : ControllerBase
+    {
+        [HttpGet()]
+        public List<int> Get([FromQuery] List<int> list)
+        {
+            var result = list.OrderByDescending(number => number).ToList();
+            return result.GetRange(0,5);
+        }
+    }
+}
