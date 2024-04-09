e20f5987a250442757165b6455db1a45cfebfbee
facundolgomez <facundolgomez87@gmail.com>
2024-04-08 21:24:55 -0300

    ej7Linq resuelto

---
+++ Web/Controllers/Ej7LinqController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Ej7LinqController : ControllerBase
+    {
+        [HttpGet()]
+        public IList<string> Get([FromQuery] string words)
+        {
+            var selectedWords = DevolverPalabrasCapitales(words);
+            return selectedWords;
+        }
+
+        public static IList<string> DevolverPalabrasCapitales(string words)
+        {
+            string[] separateWords = words.Split(' '); 
+            var upperWords = separateWords.Where(word => word == word.ToUpper());
+            return upperWords.ToList();
+
+        }
+    }
+}
+
+
+
