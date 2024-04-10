5b4c0f09949af87204fdbb016e35c5e0c40c01cc
Nicolas Arrastia <nicolasarrastia04@gmail.com>
2024-04-10 14:11:13 -0300

    Ej3Linq-resuelto

---
+++ Web/Controllers/Ej3LinqController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Ej3LinqController : ControllerBase
+    {
+        [HttpGet()]
+        public string[] Get([FromQuery] string[] stringList)
+        {
+
+            var stringQuery =
+                from word in stringList
+                where word.StartsWith("b") && word.EndsWith("r")
+                select word;
+
+            return stringQuery.ToArray();
+        }
+    }
+}
