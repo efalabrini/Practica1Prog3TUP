978e63836b0f3b647b2b3c4805773dcec9e68d21
Agustin Castelli <rev0.c4chomax@gmail.com>
2024-04-10 20:02:01 -0300

    Ejercicio5-LINQ

---
+++ Web/Controllers/Ej4LINQController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Ej4LINQController : ControllerBase
+    {
+        [HttpGet()]
+        public List<int> Get([FromQuery] int[] queryNum)
+        {
+            var list = queryNum.Take(5).OrderByDescending(x => x).ToList();
+            
+            return list;
+        }
+    }
+}
