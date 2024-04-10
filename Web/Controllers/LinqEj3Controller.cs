45b0ad0765bf38d51b1f6dd29b4b340d26621cb8
FrancoBalduini <franco233433@gmail.com>
2024-04-10 14:33:11 -0300

    linq ejercicio 3 resuelto

---
+++ Web/Controllers/LinqEj3Controller.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Ej2LinqController : ControllerBase
+    {
+        [HttpGet("consulta3")]
+        public List<string> Consulta3([FromQuery] List<string> words)
+        {
+            var result = words.Where(word => word.ToLower().StartsWith("b") && word.EndsWith("r")).ToList();
+            return result;
+        }
+    }
+}
