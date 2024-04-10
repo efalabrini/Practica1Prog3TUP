ffa88cf293c7771611fe83801dc3a289ccf27f10
Valentino Cueto <valentinocueto2005@gmail.com>
2024-04-10 14:22:26 -0300

    Ejercicio 1 resuelto

---
+++ Web/Controllers/Ej1LinqController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Ej1LinqController : ControllerBase
+    {
+        [HttpGet()]
+        public List<int> Get([FromQuery] List<int> list)
+        {
+            var query =
+                from num in list
+                where num > 30 && num < 100
+                select num;
+
+            return query.ToList(); 
+        }
+    }
+}
