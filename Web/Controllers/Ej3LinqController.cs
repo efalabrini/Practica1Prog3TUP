2b01a77738af1cc45b1ab03591a421be1ab17c62
unknown <FabricioLattante722@gmail.com>
2024-04-10 15:06:01 -0300

    Ejercicio 3 LinQ resuelto

---
+++ Web/Controllers/Ej3LinqController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Ej3LinQController : ControllerBase
+    {
+        [HttpGet()]
+        public string[] Get([FromQuery] List<string> words)
+        {
+            var queryResult = from i in words
+                              where i[0] == 'b'
+                              && i.Last() == 'r'
+                              select i;
+            return queryResult.ToArray();
+        }
+    }
+}
