d5d9aee73516a01860650d8bcf393cdaf1d67dfd
MarianoGerosa <gerosamariano@gmail.com>
2024-04-10 13:58:40 -0300

    LinQ-Ej1

---
+++ Web/Controllers/LinqEj1Controller.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LinqEj1Controller : ControllerBase
+    {
+        [HttpGet()]
+        public List<int> Get([FromQuery] List<int> list)
+        {
+            var query = from int item in list
+                        where item > 30 && item < 100
+                        select item;
+
+            return query.ToList();
+        }
+    }
+}
