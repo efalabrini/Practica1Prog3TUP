e087fdbd7d0330e9f0f5878c96ca0ea6228dc0f0
Valentino Cueto <valentinocueto2005@gmail.com>
2024-04-10 14:45:51 -0300

    Ejercicio resuelto

---
+++ Web/Controllers/Ej5LinqController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Ej5LinqController : ControllerBase
+    {
+        [HttpGet()]
+        public List<string> Get([FromQuery] List<int> list)
+        {
+            var query =
+                from num in list
+                where num * num > 20
+                select $"{num} - {num * num}";
+
+            return query.ToList();
+        }
+    }
+}
