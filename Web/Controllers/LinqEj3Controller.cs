44c00fe51f9a5d78b57efc7059256cd6b66e7e40
Julian Paquez <julianpaquez@gmail.com>
2024-04-10 14:45:34 -0300

    Ejercicio 3 linQ

---
+++ Web/Controllers/LinqEj3Controller.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LinqEj3Controller : ControllerBase
+    {
+        [HttpGet()]
+        public List<string> Get([FromQuery] List<string> palabras)
+        {
+            var query = from p in palabras
+                        where p.StartsWith("b") && p.EndsWith("r")
+                        select p;
+            return query.ToList();
+        }
+    }
+}
