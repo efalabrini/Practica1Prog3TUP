b7e0c5dcf0bd9008e0e4e03f18e34f0442fb3aef
alanmoex <alan.moex@gmail.com>
2024-04-10 11:07:48 -0300

    LinqEj6 Replace "ia"

---
+++ Web/Controllers/LinqEj6Controller.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Text.RegularExpressions;
+
+namespace Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LinqEj6Controller : ControllerBase
+    {
+        [HttpGet()]
+        public List<string> Get([FromQuery] List<string> words)
+        {
+            var result =
+                from word in words
+                select Regex.Replace(word, "ia", "*");
+                
+
+            return result.ToList();
+        }
+    }
+}
