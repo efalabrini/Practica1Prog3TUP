7dee3142921b49813db78b7149ff66c78771f4de
MarianoGerosa <gerosamariano@gmail.com>
2024-04-10 14:25:49 -0300

    LinQ-Ej3

---
+++ Web/Controllers/LinqEj3Controller.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LinqEj3Controller : ControllerBase
+    {
+        [HttpGet()]
+        public List<string> Get([FromQuery] List<string> list)
+        {
+            var query = from string w in list
+                        where w.StartsWith("b") && w.EndsWith("r")
+                        select w;
+
+            return query.ToList();
+        }
+    }
+}
