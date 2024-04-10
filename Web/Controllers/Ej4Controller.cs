a26bd3cacca9985511b47f519c8fec53d99212cf
Francisco <francisco.carballo.fc@gmail.com>
2024-04-10 15:39:11 -0300

    Ejercicio4

---
+++ Web/Controllers/Ej4Controller.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+
+namespace Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+
+
+    public class Ej4Controller : ControllerBase
+
+    {
+        [HttpGet()]
+
+        public string Get([FromQuery] int num1, [FromQuery] int num2)
+        {
+            if (num1 > num2)
+            {
+                return $"El numero {num1} es el mayor";
+            }
+
+            else if (num1 < num2)
+            {
+                return $"El numero {num2} es el mayor";
+            }
+            else
+            {
+                return $"los numeros son iguales";
+            }
+        }
+
+
+    }
+}
