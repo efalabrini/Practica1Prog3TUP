cad9666bf456fdb49cf48353794738575083b93f
Luciano Fernández <lucianofernandez122@gmail.com>
2024-04-10 14:28:39 -0300

    Ejercicio 3 pedir edad

---
+++ Web/Controllers/Ej3Controller.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class Ej3Controller : ControllerBase
+    {
+        [HttpGet()]
+        public string Get([FromQuery] string name, [FromQuery] int age)
+        {
+            return ($"Te llamas {name} y tienes {age} años.");
+        }
+    }
+}
