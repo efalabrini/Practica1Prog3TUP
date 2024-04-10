90bf8aa2c34ebce3819d97b5d4519b0d88dddb41
Luciano Fernández <lucianofernandez122@gmail.com>
2024-04-10 14:54:59 -0300

    Practica 1 Ejercicio 7 resuelto

---
+++ Web/Controllers/Ej7Controller.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class Ej7Controller : ControllerBase
+    {
+        [HttpGet]
+        public int Get() 
+        {
+            int result = 0;
+            for(int i = 0; i < 101; i++)
+            {
+                result = i;
+            }
+
+            return result;
+        }
+    }
+}
