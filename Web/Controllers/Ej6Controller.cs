49ee38b24b4e1d2a53c4d894b99c35c5cc8a53a6
Luciano Fernández <lucianofernandez122@gmail.com>
2024-04-10 14:49:07 -0300

    Practica 1 ejercicio 6 resuelto

---
+++ Web/Controllers/Ej6Controller.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class Ej6Controller : ControllerBase
+    {
+        [HttpGet()]
+        public string Get([FromQuery] float price, [FromQuery] string payment, [FromQuery] string cardNum)
+        {
+            payment = payment.Trim().ToLower();
+
+            if (payment == "efectivo")
+            {
+                return ($"el monto a pagar es de ${price}");
+            }
+            else if (payment == "tarjeta")
+            {
+                cardNum = cardNum.Trim();
+
+                if (cardNum.Length == 16)
+                {
+                    return ($"El total a pagar es de {1.1 * price}");
+                }
+                else
+                {
+                    return ("el número de tarjeta debe tener 16 caracteres");
+                }
+            }
+            else
+            {
+                return ("el metodo de pago es erroneo");
+            }
+        }
+    }
+}
