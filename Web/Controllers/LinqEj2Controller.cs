9851de29fac846de1fcc30e2ebd5e0788602599a
Luciano Fernández <lucianofernandez122@gmail.com>
2024-04-10 15:10:37 -0300

    LINQ Ejercicio 2 resuelto

---
+++ Web/Controllers/LinqEj2Controller.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class LinqEj2Controller : ControllerBase
+    {
+        [HttpGet()]
+
+        public string[] Get([FromQuery] string[] word)
+        {
+            var wordQuery =
+                from w in word
+                where w.Length > 4
+                select w.ToUpper();
+            
+            return wordQuery.ToArray();
+        }
+    }
+}
