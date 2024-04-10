4e639f8f747c80cb4aa209f368dc059bcb8f71e2
FrancoBalduini <franco233433@gmail.com>
2024-04-10 14:37:09 -0300

    linq ejercicio 4 resuelto

---
+++ Web/Controllers/LinqEj4Controller.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Ej2LinqController : ControllerBase
+    {
+        [HttpGet("consulta4")]
+        public List<int> Consulta4([FromQuery] List<int> numbers)
+        {
+            var result = numbers.OrderByDescending(num => num).Take(5).ToList();
+            return result;
+        }
+    }
+}
