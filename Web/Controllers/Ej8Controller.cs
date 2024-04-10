2beab0b078cf9b3e41476e953ac4d5802720b404
Nicolas Cataldi <nicolascataldi1@gmail.com>
2024-04-10 10:21:10 -0300

    Ejercicio 8 Hecho

---
+++ Web/Controllers/Ej8Controller.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Text;
+
+namespace Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Ej8Controller : ControllerBase
+    {
+        [HttpGet()]
+        public string Get()
+        {
+            StringBuilder sb = new StringBuilder();
+            int contador = 1;
+
+            while (contador < 101)
+            {
+                sb.Append($"\n{contador}");
+                contador++;
+            }
+            return sb.ToString();
+        }
+    }
+}
+
+//8) Recorrer los números del 1 al 100. Usar un bucle while.
