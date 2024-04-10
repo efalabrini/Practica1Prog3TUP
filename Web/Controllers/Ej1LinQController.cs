c314a4959bc589de7371075588684534fffbb536
unknown <FabricioLattante722@gmail.com>
2024-04-10 14:40:46 -0300

    Ejercicio 1 linQ resuelto

---
+++ Web/Controllers/Ej1LinQController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Ej1LinQController : ControllerBase
+    {
+        [HttpGet()]
+        public int[] Get([FromQuery] List<int> intList)
+        {
+            var result = from i in intList
+                         where i > 30
+                         && i < 100
+                         select i;
+            return result.ToArray();
+        }
+    }
+}
