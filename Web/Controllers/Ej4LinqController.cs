92ff28c37b06d0e05f40541eeb7308918edd4649
Valentino Cueto <valentinocueto2005@gmail.com>
2024-04-10 14:42:27 -0300

    Ejercicio resuelto

---
+++ Web/Controllers/Ej4LinqController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Ej4LinqController : ControllerBase
+    {
+        [HttpGet()]
+        public List<int> Get([FromQuery] List<int> list)
+        {
+            var query =
+                from num in list
+                where num > 0
+                select num;
+
+            return query.OrderByDescending(num => num).ToList();
+        }
+    }
+}
