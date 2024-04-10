0add6418cd2c7dd49bd4e71a0f324d429bab5fc1
Luciano Fernández <lucianofernandez122@gmail.com>
2024-04-10 14:43:16 -0300

    Practica 1 ejercicio 5 resuelto

---
+++ Web/Controllers/Ej5Controller.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class Ej5Controller : ControllerBase
+    {
+        [HttpGet()]
+        public string Get([FromQuery] string day)
+        {
+            day = day.Trim().ToLower();
+            if (day == "lunes" || day == "martes" || day == "miercoles" || day == "miércoles" || day == "jueves" || day == "viernes")
+            {
+                return ($"{day} es día de semana");
+            }
+            else if (day == "sabado" || day == "sábado" || day == "domingo")
+            {
+                return ($"{day} es fin de semana");
+            }
+            else
+            {
+                return ($"{day} no representa a ningún día");
+            }
+        }
+    }
+}
