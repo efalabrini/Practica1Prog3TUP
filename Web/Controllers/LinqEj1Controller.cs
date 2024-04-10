7bc5b5926a2557f5a4bc961ca76c2847ecc63086
Luciano Fernández <lucianofernandez122@gmail.com>
2024-04-10 15:08:01 -0300

    LINQ Ejercicio 1 resuelto

---
+++ Web/Controllers/LinqEj1Controller.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class LinqEj1Controller : ControllerBase
+    {
+        [HttpGet()]
+        public int[] Get()
+        {
+            int[] numbers = [67, 92, 153, 15];
+
+            var numQuery =
+                from num in numbers
+                where (num > 30 && num < 100)
+                select num;
+
+            return numQuery.ToArray();
+        }
+    }
+}
