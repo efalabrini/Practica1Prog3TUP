2789676d651e69afa2a7dac79c56cc8572f224be
Luciano Fernández <lucianofernandez122@gmail.com>
2024-04-10 14:40:28 -0300

    Practica 1 ejercicio 4 mayor menor

---
+++ Web/Controllers/Ej4Controller.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class Ej4Controller : ControllerBase
+    {
+        [HttpGet()]
+        public string Get([FromQuery] int number1, [FromQuery] int number2)
+        {
+            if(number1 > number2)
+            {
+                return ($"{number1} es mayor que {number2}");
+            }else if (number1 < number2)
+            {
+                return ($"{number2} es mayor que {number1}");
+            } else
+            {
+                return ("Ambos numeros son iguales");
+            }
+        }
+    }
+}
