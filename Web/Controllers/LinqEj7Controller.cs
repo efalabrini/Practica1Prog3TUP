95c1cbb4f4505ce2b5db24a2931b9947cfae5200
alanmoex <alan.moex@gmail.com>
2024-04-10 11:40:53 -0300

    LinqEj7 Capital Words

---
+++ Web/Controllers/LinqEj7Controller.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LinqEj7Controller : ControllerBase
+    {
+        [HttpGet()]
+        public IList<string> Get([FromQuery] string words)
+        {
+            return DevolverPalabrasCapitales(words);
+        }
+
+        public static IList<string> DevolverPalabrasCapitales(string cadena)
+        {
+            var splitedCadena = cadena.Split(' ');
+            
+            var result = 
+                from word in splitedCadena
+                where word.Equals(word.ToUpper())
+                select word;
+
+            return result.ToList();
+        }
+    }
+}
