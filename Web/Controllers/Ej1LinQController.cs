a2a7d66a5399dfd34d88a79510b8bbc050dbbbad
Aylen Guy <ayluguy@gmail.com>
2024-04-10 14:57:37 -0300

    Ejercicio 1 LinQ

---
+++ Web/Controllers/Ej1LinQController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Ej1LinQController : ControllerBase
+    {
+        [HttpGet]
+        public List<int> Get([FromQuery] List<int> List)
+        {
+
+            var filteredresult = List.Where(x => x > 30 && x < 100).ToList();
+
+
+            return filteredresult;
+        }
+    }
+
+}
