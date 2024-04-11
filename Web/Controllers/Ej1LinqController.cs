b87275a81b50b8a0aca0a5c13612026007467a16
Lucaslovera24 <lucaslovera24@hotmail.com>
2024-04-11 16:30:19 -0300

    Ejercicio 1 Linq terminado

---
+++ Web/Controllers/Ej1LinqController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Ej1LinqController : ControllerBase
+    {
+        [HttpGet()]
+        public List<int> Get([FromQuery] List<int> onelist)
+        {
+            var numbers = onelist.Where(x => x > 30 && x < 100).ToList();
+            return numbers;
+        }
+    }
+}
