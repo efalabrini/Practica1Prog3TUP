1a779bd54f34a58f0c8d5ec28ff17878fc07e1d1
Francisco <francisco.carballo.fc@gmail.com>
2024-04-11 14:24:57 -0300

    Practica1 - Ejc5

---
+++ Web/Controllers/Ej5Controller.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+
+    public class Ej5Controller : ControllerBase
+
+    {
+        [HttpGet()]
+
+        public string Get([FromQuery] string days)
+        {
+
+            string[] week = ["lunes", "martes", "miércoles", "jueves", "viernes"];
+            string[] weekeend = [ "sábado", "domingo"] ;
+
+            days = days.ToLower().Trim();
+
+            if (week.Contains(days)) 
+            {
+                return $"{days} es un dia de semana";
+            }
+            else if (weekeend.Contains(days))
+            {
+                return $"{days} es un dia del fin de semana";
+            }
+            else
+            {
+                return "Error, ese dia no existe";
+            }
+        }
+    }
+}
