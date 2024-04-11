a29c7ac4f3663c7be848338c37d37ffb592f1061
JoaquinBenitezVelazquez <velazquezjoaquin163@gmail.com>
2024-04-11 11:06:55 -0300

    ej4-Resuelto

---
+++ Web/Controllers/Ej5Controller.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+
+
+namespace Web.Controllers
+{
+
+
+    [Route("api/[controller]")]
+    [ApiController]
+
+    public class Ej5Controller : ControllerBase
+    {
+
+    [HttpGet()]
+
+     public string Get([FromQuery] string nombreDia)
+      
+
+        {
+            string respuesta;
+
+
+            if (nombreDia == "lunes" || nombreDia == "martes" || nombreDia == "miercoles" || nombreDia == "jueves" || nombreDia == "viernes")
+
+            { 
+                respuesta = "Es un dia de la semana";
+             }
+
+       
+           else if (nombreDia == "sabado" || nombreDia =="domingo")
+            {
+                respuesta = "NO es un dia de la semana";
+            }
+
+
+            else
+            {
+                respuesta = "hay un error flaco"; 
+            }
+
+
+            return respuesta;
+        
+        
+        
+        
+        
+        }
+
+
+
+
+
+
+
+    }
+}
