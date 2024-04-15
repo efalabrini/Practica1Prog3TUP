5b271d08d235a3b8f83a796c8c4824f3c29cd5ac
Luciano Fernández <lucianofernandez122@gmail.com>
2024-04-14 21:43:39 -0300

    LINQ Ejercicio 4 resuelto

---
+++ Web/Controllers/LinqEj4Controller.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class LinqEj4Controller : ControllerBase
+    {
+        [HttpGet()]
+        public int[] Get([FromQuery] int[] numbers)
+        {
+            Array.Sort(numbers);
+            Array.Reverse(numbers);
+
+            var numQuery =
+                from n in numbers
+                where n > 0
+                select n;
+
+            return numQuery.ToArray();
+        }
+    }
+}
