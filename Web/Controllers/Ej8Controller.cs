dbe282878c46b10c28cbdb1028b044e8fbb2a6b9
Francisco <francisco.carballo.fc@gmail.com>
2024-04-22 18:26:20 -0300

    Ejc8/P1

---
+++ Web/Controllers/Ej8Controller.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text;
+
+namespace Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+
+
+    public class Ej8Controller : ControllerBase
+    {
+        [HttpGet()]
+
+        public string Get()
+        {
+            StringBuilder Numbers = new StringBuilder();  // Constructor de cadenas
+
+            int Cont = 0;
+
+            while (Cont <= 99)
+            {
+                Cont++;
+                Numbers.Append($"{Cont} - ");
+            }
+
+           
+            return Numbers.ToString();
+
+
+        }
+    }
+}
