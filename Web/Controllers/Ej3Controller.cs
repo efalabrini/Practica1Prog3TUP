ad3989a40cff56714072504d36e23572a4e6aca7
NicoBertoni1 <nicobertoni05@gmail.com>
2024-04-23 16:45:29 -0300

    Ejercicio 3

---
+++ Web/Controllers/Ej3Controller.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Ejercicio3 : ControllerBase
+    {
+        [HttpGet()]
+        public string Get([FromQuery] string name, [FromQuery] string age)
+        {
+            string YourDates = $"Te llamas {name} y tienes: {age} años.";
+            return YourDates;
+        }
+    }
+}
+
