116f6232867af3e03c01fdf5fc3cd2a4bcf89634
MateoBernardi5 <mateoobernardi@gmail.com>
2024-04-24 11:42:41 -0300

    Resolución ejercicio 3 de LINQ

---
+++ Web/Controllers/Ej3LinqController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Ej3LinqController : ControllerBase
+    {
+        [HttpGet()]
+        public List<string> Get([FromQuery] List<string> lista)
+        {
+            var wordQuery =
+                from word in lista
+                where word.StartsWith("b") && word.EndsWith("r")
+                select word;
+
+            return wordQuery.ToList();
+        }
+    }
+}
