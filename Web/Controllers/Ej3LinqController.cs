38cf143a1557189ed194fbc5d9685e88abb8d4ec
JuaniTorti <nachi.tortjuani@gmail.com>
2024-04-24 13:54:51 -0300

    Update Ej3LinqController.cs

--- Web/Controllers/Ej3LinqController.cs
+++ Web/Controllers/Ej3LinqController.cs
@@ -12,9 +12,13 @@ namespace Web.Controllers
         [HttpGet()]
         public List<string> Get([FromQuery] List<string> list)
         {
-            var result = list.Where(word => word.Trim().ToLower()[0] == 'b' && word.Trim().ToLower()[word.Trim().Length - 1] == 'r').ToList(); 
-                                            // si no ponia asi el subindice y ponia un espacio al final de la palabra daba error xq con los metodos no se midifica la longitud
-                                            // y al querer usar la longitud de la palaba sin los metodos me daba una long mas grande y se salia de rango
+            var result = list
+                .Where(
+                    word => word.Trim().ToLower()[0] == 'b' 
+                    && 
+                    word.Trim().ToLower()[word.Trim().Length - 1] == 'r'
+                    )
+                .ToList(); 
             return result;
 
         }
