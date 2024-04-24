8668b808ff352193f58cca03757c31b0ecd8bedc
NicoBertoni1 <nicobertoni05@gmail.com>
2024-04-24 10:44:27 -0300

    Ejercicio 4

---
+++ Web/Controllers/Ej4Controller.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics.Eventing.Reader;
+
+namespace Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Ejercicio4 : ControllerBase
+    {
+        [HttpGet()]
+        public IActionResult Get([FromQuery] int num1, [FromQuery] int num2)
+        {
+            string message;
+
+            if (num1 > num2)
+            {
+                message = "El número 1 es mayor que el número 2";
+            }
+            else if (num1 < num2)
+            {
+                message = "El número 2 es mayor que el número 1";
+            }
+            else
+            {
+                message = "El número 1 es igual al número 2";
+            }
+
+            return Ok(message);
+        }
+    }
+}
