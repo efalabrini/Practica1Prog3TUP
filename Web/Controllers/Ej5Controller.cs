3683786dd6887643bb3f1fb1b632f96918b2940c
NicoBertoni1 <nicobertoni05@gmail.com>
2024-04-24 10:53:20 -0300

    Ejercicio 5

---
+++ Web/Controllers/Ej5Controller.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using System;
+
+namespace Proyecto1Programacion3.Controllers
+{
+    public class Ejercicio_5 : Controller
+    {
+        public string Get([FromQuery] string day)
+        {
+            string reply = "Fin de semana";
+            day = day.ToLower();
+            string[] week = new string[] { "lunes", "martes", "miercoles", "jueves", "viernes", "sabado", "domingo" };
+
+            bool containsDaysWeek = week.Any(days => day.Trim().contains(days, StringComparasion.OrdinalIgnoreCase))
+        }
+    }
+}
