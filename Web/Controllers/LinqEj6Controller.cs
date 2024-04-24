3ac96d67e0b206a177feef8e5a9df18830d49cb9
Luciano Fernández <lucianofernandez122@gmail.com>
2024-04-24 20:38:50 -0300

    LINQ Ejercicio 6 resuelto

---
+++ Web/Controllers/LinqEj6Controller.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class LinqEj6Controller : ControllerBase 
+    {
+        [HttpGet]
+        public List<string> Get([FromQuery] List<string> lista)
+        {
+            var query =
+                from palabras in lista
+                select palabras.Replace("ia", "*");
+
+            return query.ToList();
+        }
+    }
+}
